842d1e073c9593f74e734074e0f9446f2bb63f95
leminhluan4244 <leminhluan4244@gmail.com>
2017-11-24 18:44:06 +0700

    update
    update

--- SudokuN/SudokuN/Controller/Class10.cs
+++ SudokuN/SudokuN/Controller/Class10.cs
@@ -79,8 +79,8 @@ namespace SudokuN.Controller
                         }
                         catch
                         {
-                            num[i, j].Value = -1;
                             MessageBox.Show("Error Row " + (i + 1) + " and Colum " + (j + 1), "Check Maxtrix", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            return array;
                         }
                     }
                 }
